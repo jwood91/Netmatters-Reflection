b5a13d37d820baacbdd2c1c0141ed3ade5a183f4
Joe Wood <jwood4589@gmail.com>
2021-06-14 17:04:24 +0100

    final handin commits

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,120 @@
+$(document).ready(function() {
 
-/* toggle the side navigation and the hamburger transform on click */
-function toggleNav() {
-    $(`#sideNavContainer`).toggleClass('isOpen');
-    $(`#hamburger`).toggleClass('is-active');
+  /* Image Slider */
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    rows: 1,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+
+  });
+
+});
+
+/* Cookie */
+
+const cookieStorage = {
+    getItem: (item) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+        return cookies[item];
+    },
+    setItem: (item, value) => {
+        document.cookie = `${item}=${value};`
+    }
+}
+
+const storageType = cookieStorage;
+const consentPropertyName = 'netmatters_consent';
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+
+    const acceptFn = event => {
+        saveToStorage(storageType);
+        overlaySeen.classList.remove('show-overlay');
+        consentPopup.classList.add('hidden');
+
+    }
+    const consentPopup = document.getElementById('consent-popup');
+    const acceptBtn = document.getElementById('cookie-accept');
+    const overlaySeen = document.getElementById('overlay');
+    acceptBtn.addEventListener('click', acceptFn);
+
+    if (shouldShowPopup(storageType)) {
+        setTimeout(() => {
+          overlaySeen.classList.add('show-overlay');
+          overlaySeen.classList.add('cookie-overlay');
+          consentPopup.classList.remove('hidden');
+
+        }, 200);
     }
 
+};
 
 
 
-// $(document).ready(function(){
-//   $("#sticker").sticky({topSpacing:0});
-// });
+
+/* script for the sticky nav bar */
+
+let headerHeight = $('#scroll-header').outerHeight();
+let lastScrollTop = 0;
+
+$('#scroll').scroll(function() {
+      clearTimeout($.data(this, 'scrollTimer'));
+      $.data(this, 'scrollTimer', setTimeout(function() {
+          let st = $('#scroll').scrollTop();
+          /*scroll down */
+          if (st > lastScrollTop && st > headerHeight) {
+            if ($('#scroll-header').hasClass('nav-up') || $('#scroll-header').hasClass('nav-down-shown')  ) {
+              $('#scroll-header').removeClass('nav-up').removeClass('nav-down-shown').addClass('nav-down')
+              console.log("scroll down conditional complete")
+            }
+          }
+          /* scroll at page top */
+          else if (st == 0 && $(`#scroll-header`).hasClass('nav-down')) {
+            $('#scroll-header').removeClass('nav-down').addClass('nav-up');
+            console.log("scrolled  to top conditional complete")
+            }
+            else if (st <= headerHeight) {
+              /* empty to stop next conditional firing */
+
+            } else if (st < lastScrollTop && st > headerHeight) {
+              if ($('#scroll-header').hasClass('nav-down')) {
+                $('#scroll-header').removeClass('nav-down').addClass('nav-down-shown')
+              }
+            }
+            console.log("Haven't scrolled in 250ms!");
+            lastScrollTop = st;
+          }, 250));
+      }
+    );
+
+
+
+
+
+
+    /* toggle the side navigation and the hamburger transform on click */
+    function toggleNav() {
+      $('#overlay').toggleClass('show-overlay');
+      $('#overlay').toggleClass('overlay-menu');
+      $(`#overlay`).toggleClass('isOpen');
+      $(`#scroll-header`).toggleClass('isOpen');
+      $(`#mainContent`).toggleClass('isOpen');
+      $(`#hamburger`).toggleClass('is-active');    }
+
+    function sideNavHide() {
+      if ($(`#mainContent`).hasClass('isOpen')) {
+        $(`#overlay`).removeClass('show-overlay')
+        $(`#overlay`).removeClass('overlay-menu');
+        $(`#overlay`).removeClass('isOpen');
+        $(`#scroll-header`).removeClass('isOpen');
+        $(`#mainContent`).removeClass('isOpen');
+        $(`#hamburger`).removeClass('is-active');
+      }
+    }
